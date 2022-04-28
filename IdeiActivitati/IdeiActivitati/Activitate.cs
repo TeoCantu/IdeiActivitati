a780a561f234f23757a57a3028ccc37232033f2f
TeoCantu <teocantu@gmail.com>
2022-04-28 09:52:55 +0300

    Add Activitate.cs, listaActivitati for initial usage

---
+++ IdeiActivitati/IdeiActivitati/Activitate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IdeiActivitati
+{
+    public class Activitate
+    {
+        public int Id { get; set; }
+        public string Descriere { get; set; }
+        public string Tip { get; set; }
+        // tipuri activitati ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+        public int Participanti { get; set; }
+        public double Cost { get; set; }
+        public DateTime Data { get; set; }
+        public string DetaliiSuplimentare { get; set; }
+
+        public Activitate(int id, string descriere, string tip, int participanti, double cost, DateTime data, string detaliiSuplimentare)
+        {
+            Id = id;
+            Descriere = descriere;
+            Tip = tip;
+            Participanti = participanti;
+            Cost = cost;
+            Data = data;
+            DetaliiSuplimentare = detaliiSuplimentare;
+        }
+    }
+}
